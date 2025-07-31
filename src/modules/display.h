9217a6d46d03ea97dd65610250a8d32fbb7da946
purin <puriso@users.noreply.github.com>
2025-08-01 00:49:32 +0900

    Add oil temp duration comment

--- src/modules/display.h
+++ src/modules/display.h
@@ -9,6 +9,7 @@
 extern M5GFX display;
 extern M5Canvas mainCanvas;
 extern int currentFps;
+// 油温が120°C以上だった累積時間 [ms]
 extern unsigned long oilTempOver120TimeMs;
 
 void drawOilTemperatureTopBar(M5Canvas& canvas, float oilTemp, int maxOilTemp);
